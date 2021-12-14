e4c0d94d789f7107c91017af4bf3c82571bb6de8
Ubuntu <ubuntu@ip-172-26-8-9.ap-northeast-2.compute.internal>
2021-12-15 02:46:52 +0900

    e

--- config/settings/base.py
+++ config/settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = my_settings.SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['52.78.93.108']
 
